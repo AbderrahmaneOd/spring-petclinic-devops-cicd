bee5fa269213e9d32ebb62afd43bd1fde593602a
Abderrahmane Ouaday <abdoouday361@gmail.com>
2024-06-15 17:27:15 +0100

    Fix formatting

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -22,7 +22,6 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportRuntimeHints;
 
-
 /**
  * PetClinic Spring Boot Application.
  *
